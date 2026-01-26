73e5325b5fb16b9ef0dcca17d35cdc3af2c17597
ArikSquad <75741608+ArikSquad@users.noreply.github.com>
2026-01-26 20:55:28 +0200

    it's 2026

--- src/main/java/eu/mikart/adventure/platform/hytale/package-info.java
+++ src/main/java/eu/mikart/adventure/platform/hytale/package-info.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of adventure-platform-hytale, licensed under the MIT License.
  *
- * Copyright (c) 2025 ArikSquad
+ * Copyright (c) 2026 ArikSquad
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
